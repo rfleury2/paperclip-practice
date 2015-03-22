3f7d4b5727f01c49bf270a35f0bd8f7fe4612e31
Ricardo Fleury <rfleury2@gmail.com>
2015-03-22 13:24:16 -0500

    Add model creation and validation for file attachment feature

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,8 @@
 class Team < ActiveRecord::Base
 
+  has_attached_file :shield, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment_content_type :shield, :content_type => /\Aimage\/.*\Z/
+
   validates :name, presence: true
   validates :city, presence: true
   validates :country, presence: true
