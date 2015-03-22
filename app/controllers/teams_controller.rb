f4cde346d99d37e098d36b1f8796815afc956ba7
Ricardo Fleury <rfleury2@gmail.com>
2015-03-22 12:42:12 -0500

    Add create team logic and validations

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,4 +5,20 @@ class TeamsController < ApplicationController
 
   def new
   end
+
+  def create
+    team = Team.create(team_params)
+    if team.valid?
+      redirect_to teams_path
+    else
+      @errors = team.errors
+      render 'new'
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :city, :country, :coach)
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,8 @@
 class Team < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :coach, presence: true
+
 end
