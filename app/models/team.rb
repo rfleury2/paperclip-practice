210ff6f733faf8602182abb61173cf2d91c5ee15
Ricardo Fleury <rfleury2@gmail.com>
2015-03-22 13:54:08 -0500

    Add barcelona and real madrid files for testing

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,11 +1,15 @@
 class Team < ActiveRecord::Base
 
-  has_attached_file :shield, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :shield, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/images/missing.png"
   validates_attachment_content_type :shield, :content_type => /\Aimage\/.*\Z/
 
   validates :name, presence: true
   validates :city, presence: true
   validates :country, presence: true
   validates :coach, presence: true
+
+  validates_attachment_presence :shield
+  validates_attachment_size :shield, less_than: 2.megabytes
+  validates_attachment_content_type :shield, content_type: ['image/jpeg', 'image/png', 'image/svg']
 
 end
